0a29bb5d4a600c18d2af1e2a6a36f6087a2fcfc9
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-07 17:05:38 +0200

    creo la funzione

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log('JS OK');
+
+// TODO recupero e scrivo gli elementi utili
+
+//pulsate Play
+const playButton = document.getElementById('play');
+//griglia
+const fullGrid = document.getElementById('grid');
+
+const rows = 10; //righe
+const columns = 10; //colonne
+const cellsNumber = rows * columns; //totale celle
+
+//creazione della griglia
+for(i = 0; i < cellsNumber; i++){
+    const cell = document.createElement('div'); // creo i div figli di grid
+    cell.className = 'col'; // 'col' per classe di Bootstrap
+    cell.innerText = i + 1;
+    cell.addEventListener('click', function(){ // se premo il pulsante
+        console.log(); //scrivo il numero in log
+        cell.classList.add('azure'); //diventa azzurro
+    });
+
+    fullGrid.appendChild(cell); // inserisco in pagina le celle
+}
