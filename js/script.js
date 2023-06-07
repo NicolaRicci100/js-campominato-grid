da2689feece1ddfe84ede8498645f161abf52d84
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-07 17:18:20 +0200

    attivo il pulsante play

--- js/script.js
+++ js/script.js
@@ -10,16 +10,17 @@ const fullGrid = document.getElementById('grid');
 const rows = 10; //righe
 const columns = 10; //colonne
 const cellsNumber = rows * columns; //totale celle
+playButton.addEventListener('click', function(){ // attivo il pulsante play
+    //creazione della griglia
+    for(i = 0; i < cellsNumber; i++){
+        const cell = document.createElement('div'); // creo i div figli di grid
+        cell.className = 'col'; // 'col' per classe di Bootstrap
+        cell.innerText = i + 1;
+        cell.addEventListener('click', function(){ // se premo il pulsante
+            console.log(); //scrivo il numero in log
+            cell.classList.add('azure'); //diventa azzurro
+        });
 
-//creazione della griglia
-for(i = 0; i < cellsNumber; i++){
-    const cell = document.createElement('div'); // creo i div figli di grid
-    cell.className = 'col'; // 'col' per classe di Bootstrap
-    cell.innerText = i + 1;
-    cell.addEventListener('click', function(){ // se premo il pulsante
-        console.log(); //scrivo il numero in log
-        cell.classList.add('azure'); //diventa azzurro
-    });
-
-    fullGrid.appendChild(cell); // inserisco in pagina le celle
-}
+        fullGrid.appendChild(cell); // inserisco in pagina le celle
+    }
+})
